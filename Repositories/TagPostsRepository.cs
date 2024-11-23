207ebc02a5691ea8dc1bc27edfd73056aee8efdc
RdAnjos <rodrigo.oda@hotmail.com>
2024-11-22 21:48:17 -0300

    Created list Tags with Posts' quantity

---
+++ Repositories/TagPostsRepository.cs
@@ -0,0 +1,58 @@
+using Blog.Models;
+using Blog.Repositories;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace blog.Repositories
+{
+    public class TagPostsRepository : Repository<Tag>
+    {
+        private readonly SqlConnection _connection;
+
+        public TagPostsRepository(SqlConnection connection) : base()
+           => _connection = connection;
+
+        public List<Tag> ListTagAndPostsQuantity()
+        {
+            var query = @"SELECT 
+                            [Tag].*,
+                            [Post].[Id],
+                            COUNT([Post].[Id]) AS [Posts]
+                        FROM 
+                            [Tag]
+                        INNER JOIN [PostTag] ON [PostTag].[TagId] = [Tag].[Id]
+                        INNER JOIN [Post] ON [PostTag].[PostId] = [Post].[Id]
+                        GROUP BY     
+                            [Tag].[Id],
+                            [Tag].[Name],
+                            [Tag].[Slug],
+                            [Post].[Id]";
+            
+            var listTags = new List<Tag>();
+
+            var items = _connection.Query<Tag, Post, Tag>(
+                query,
+                (tag, post) => 
+                {
+                    var tags = listTags.FirstOrDefault(t => t.Id == tag.Id);
+                    
+                    if (tags == null)
+                    {
+                        tags = tag;
+                        if (post != null)
+                        {
+                            tags.Posts.Add(post);
+                        }
+                        listTags.Add(tags);
+                    }
+                    else 
+                    {
+                        tags.Posts.Add(post);
+                    }
+                    return tag;
+                }, splitOn: "Id");
+            return listTags;
+        }
+        
+    }
+}
---
+++ Screens/PostTagScreens/ListTagWithPostsQuantityScreen.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using blog.Repositories;
+using Microsoft.Data.SqlClient;
+
+namespace Blog.Screens.PostTagScreens
+{
+    public class ListTagWithPostsQuantityScreen
+    {
+        public static void Init()
+        {
+            ListTagWithPostsQuantity(Database.Connection);
+        }
+        private static void ListTagWithPostsQuantity(SqlConnection connection)
+        {
+            var tagsAndPosts = new TagPostsRepository(connection);
+            var tags = tagsAndPosts.ListTagAndPostsQuantity();
+
+            foreach (var tag in tags)
+            {
+
+                    System.Console.WriteLine();
+                    System.Console.WriteLine($"Tag ID: {tag.Id}, Tag Name: {tag.Name}, Posts: {tag.Posts.Count}");
+                    System.Console.WriteLine("-------------------------------------------------------");
+                
+            }
+        }
+    }
+}
